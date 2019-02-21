aad796a91b22ed835fd15b9aa9f23a9b61560e20
Ethan Martin <Martin.Ethan@pennmutual.com>
2019-02-20 21:16:48 -0500

    Initial commit for 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # eslint-config-pilatch
 No-nonsense ESLint configuration emphasizing a lack of stupidity
+
+This is a [sharable ESLint configuration](https://eslint.org/docs/developer-guide/shareable-configs).
+
+## Use
+
+In your .eslintrc.json file for your project, have the following:
+
+    {
+      "extends": "pilatch"
+    }
---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "extends": "standard",
+  "rules": {
+    "comma-dangle": ["error", "always-multiline"],
+    "consistent-this": ["error", "self"],
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "newline-after-var": ["error", "always"],
+    "no-else-return": ["error", {"allowElseIf": false}],
+    "no-implicit-coercion": "error",
+    "no-lonely-if": "error",
+    "no-shadow": "error",
+    "no-useless-return": "error",
+    "object-curly-spacing": ["error", "never"],
+    "quote-props": ["error", "consistent"],
+    "space-before-function-paren": ["error", "never"],
+    "vars-on-top": "error"
+  }
+}
